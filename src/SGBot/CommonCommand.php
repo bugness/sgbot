ca284f5e54a24777895ff30556b3b627eb7f5c77
bugness <bugness@triganz.org>
2014-03-09 21:43:24 +0800

    Crawler component added

--- src/SGBot/CommonCommand.php
+++ src/SGBot/CommonCommand.php
@@ -5,16 +5,37 @@ namespace SGBot;
 use Symfony\Component\Console\Command\Command;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Output\OutputInterface;
+use Guzzle\Http\Client;
+use Symfony\Component\DomCrawler\Crawler;
 
 class CommonCommand extends Command
 {
     protected function configure()
     {
-        $this->setName('common:demo');
+        $this->setName('common:test');
     }
 
     protected function execute(InputInterface $input, OutputInterface $output)
     {
-        $output->writeln('test');
+        $client = new Client('http://localhost:8000');
+        $request = $client->get('/list.html');
+//        $request->addCookie('PHPSESSID', '');
+//        $request->addCookie('expires', 'Sun, 16-Mar-2014 09:22:24 GMT');
+//        $request->addCookie('path', '/');
+//        $request->addCookie('domain', '.example.com');
+        $response = $request->send();
+
+        $output->writeln('-----------------------');
+
+        $crawler = new Crawler($response->getBody(true));
+        $posts = $crawler
+            ->filter('div.post:not(.fade) > div.left > div.title > a')
+            ->each(function (Crawler $node, $i) {
+                return $node->text() . ' - ' . $node->attr('href');
+            }
+        );
+        $output->writeln(join(PHP_EOL, $posts));
+
+        $output->writeln('-----------------------');
     }
 }
