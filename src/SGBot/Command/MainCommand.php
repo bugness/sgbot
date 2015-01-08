bba64993a51f45ab453bbee51d79d6d5c6cce7b1
bugness <bugness@triganz.org>
2015-01-08 16:39:54 +0700

    refactoring

---
+++ src/SGBot/Command/MainCommand.php
@@ -0,0 +1,54 @@
+<?php
+
+namespace SGBot\Command;
+
+use Symfony\Component\Console\Command\Command;
+use Symfony\Component\Console\Input\InputArgument;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Output\OutputInterface;
+use Symfony\Component\Yaml\Parser;
+use Symfony\Component\Yaml\Exception\ParseException;
+use SGBot\Service\Provider;
+
+class MainCommand extends Command
+{
+    protected function configure()
+    {
+        $this
+            ->setName('app:exec')
+            ->addArgument('config', InputArgument::REQUIRED)
+        ;
+    }
+
+    protected function execute(InputInterface $input, OutputInterface $output)
+    {
+        $file = $input->getArgument('config');
+        if (!file_exists($file)) {
+            $output->writeln("File '{$file}' not found");
+            return;
+        }
+
+        $yaml = new Parser;
+        try {
+            $config = $yaml->parse(file_get_contents($file));
+        } catch (ParseException $e) {
+            $output->writeln("Unable to parse the YAML string: {$e->getMessage()}");
+            return;
+        }
+
+        $provider = new Provider($config);
+        $result = $provider->enterToGiveaways();
+
+        if (!count($result)) {
+            return;
+        }
+
+        array_unshift($result, '----', date('d M Y H:i:s'));
+
+        file_put_contents(
+            APP_PATH . DIRECTORY_SEPARATOR . $config['username'] . '.log',
+            join(PHP_EOL, $result),
+            FILE_APPEND
+        );
+    }
+}
